8ae09ddafdcd9f68ebd116c1c924b9c8ebec1276
Gaurav Kumar <gauravkr2179@gmail.com>
2024-05-08 20:35:11 +0530

    Update orderRoute.js

--- order/src/routes/orderRoute.js
+++ order/src/routes/orderRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const orderController = require('../controllers/orderController')
 const paymentController = require('../controllers/paymentController')
+const {verifyUser} = require('../middelware/verifyUser')
 const router = express.Router();
 
+router.use(verifyUser)
 router.post('/productOrder', orderController.createOrder)
 router.post('/payment', paymentController.paymentOrder)
 router.post('/webhooks', paymentController.verifyPaymentWebhook)
